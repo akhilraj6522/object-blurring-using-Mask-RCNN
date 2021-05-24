4f75591bbdda9ae26e1d94e397d5eeedab85d421
Akhil Raj <76519326+akhilraj6522@users.noreply.github.com>
2021-05-24 23:14:43 +0530

    Update parallel_model.py

--- mrcnn/parallel_model.py
+++ mrcnn/parallel_model.py
@@ -1,10 +1,4 @@
 """
-Mask R-CNN
-Multi-GPU Support for Keras.
-
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
 
 Ideas and a small code snippets from these sources:
 https://github.com/fchollet/keras/issues/2436
